0a8e5f47da646cb10557126006e2397ad59bf82a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to print ChartControl using SimpleLink (DXPrinting)
+
+
+<p>This example illustrates an approach of including a <strong>ChartControl </strong>picture into a document generated via SimpleLink (DXPrinting).</p><p>A corresponding functionality should be available out-of-the-box in one of future updates (see <a href="http://www.devexpress.com/issue=S32927">http://www.devexpress.com/issue=S32927</a>).<br />
+In the example, a chart's image is obtained via the  <strong>WriteableBitmap.Render</strong> method.</p>
+
+<br/>
+
+
---
+++ VB/ChartPrinting/MainPage.xaml.vb
@@ -0,0 +1,50 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports System.Windows.Media
+Imports System.Windows.Controls
+Imports System.Windows.Media.Imaging
+Imports DevExpress.Xpf.Printing
+
+Namespace ChartPrinting
+    Partial Public Class MainPage
+        Inherits UserControl
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim sl As New SimpleLink()
+            Dim model As New LinkPreviewModel(sl)
+            sl.ExportServiceUri = "../ReportService1.svc"
+            documentPreview1.Model = model
+            sl.DetailCount = 1
+            sl.Detail = CType(Resources("Data"), DataTemplate)
+            AddHandler sl.CreateDetail, AddressOf sl_CreateDetail
+            sl.CreateDocument(False)
+            tabControl1.SelectedItem = tabItem2
+            tabItem2.Visibility = Visibility.Visible
+        End Sub
+
+        Private Sub sl_CreateDetail(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
+            Dim wb As _
+                New WriteableBitmap(CInt(Fix(chartControl1.ActualWidth)), CInt(Fix(chartControl1.ActualHeight)))
+
+            wb.Render(chartControl1, New TranslateTransform())
+
+            wb.Invalidate()
+            e.Data = wb
+        End Sub
+
+        Private Sub tabItem1_GotFocus(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            tabItem2.Visibility = Visibility.Collapsed
+            button1.IsEnabled = True
+        End Sub
+
+        Private Sub tabItem2_GotFocus_1(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            button1.IsEnabled = False
+        End Sub
+
+    End Class
+
+End Namespace
